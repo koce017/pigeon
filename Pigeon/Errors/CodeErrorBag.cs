1142249d9ad1ce9b1a0b97edf6e63f59d3ed63d4
koce017 <koce017@mail.com>
2025-09-30 14:44:34 +0200

    Tweak error messages

--- Pigeon/Errors/CodeErrorBag.cs
+++ Pigeon/Errors/CodeErrorBag.cs
@@ -15,12 +15,12 @@ namespace Kostic017.Pigeon.Errors
 
         internal void ReportUndeclaredVariable(TextSpan textSpan, string name)
         {
-            Report($"The variable '{name}' does not exist in the current contex", textSpan);
+            Report($"The variable '{name}' does not exist in the current context", textSpan);
         }
 
         internal void ReportUndeclaredFunction(TextSpan textSpan, string name)
         {
-            Report($"The function '{name}' does not exist in the current contex", textSpan);
+            Report($"The function '{name}' does not exist in the current context", textSpan);
         }
 
         internal void ReportVariableRedeclaration(TextSpan textSpan, string name)
@@ -30,7 +30,7 @@ namespace Kostic017.Pigeon.Errors
 
         internal void ReportInvalidTypeAssignment(TextSpan textSpan, string variableName, PigeonType variableType, PigeonType valueType)
         {
-            Report($"Variable '{variableName}' of type {variableType.Name} cannot have value of type {valueType.Name}", textSpan);
+            Report($"Variable '{variableName}' of type '{variableType.Name}' cannot have value of type '{valueType.Name}'", textSpan);
         }
 
         internal void ReportDuplicatedArgument(TextSpan textSpan, string parameterName)
@@ -40,17 +40,17 @@ namespace Kostic017.Pigeon.Errors
 
         internal void ReportInvalidTypeUnaryOperator(TextSpan textSpan, string op, PigeonType type)
         {
-            Report($"Operator {op} is not defined for type {type.Name}", textSpan);
+            Report($"Operator '{op}' is not defined for type {type.Name}", textSpan);
         }
 
         internal void ReportInvalidTypeBinaryOperator(TextSpan textSpan, string op, PigeonType type1, PigeonType type2)
         {
-            Report($"Operator {op} is not defined for types {type1.Name} and {type2.Name}", textSpan);
+            Report($"Operator '{op}' is not defined for types '{type1.Name}' and '{type2.Name}'", textSpan);
         }
 
         internal void ReportInvalidTypeTernaryOperator(TextSpan textSpan, PigeonType type1, PigeonType type2)
         {
-            Report($"Types {type1.Name} and {type2.Name} are not compatible", textSpan);
+            Report($"Types '{type1.Name}' and '{type2.Name}' are not compatible", textSpan);
         }
 
         internal void ReportRedefiningReadOnlyVariable(TextSpan textSpan, string name)
@@ -60,32 +60,32 @@ namespace Kostic017.Pigeon.Errors
 
         internal void ReportUnexpectedType(TextSpan textSpan, PigeonType actualType)
         {
-            Report($"Got value of unexpected type {actualType.Name}", textSpan);
+            Report($"Got value of unexpected type '{actualType.Name}'", textSpan);
         }
 
         internal void ReportUnexpectedType(TextSpan textSpan, PigeonType expectedType, PigeonType actualType)
         {
-            Report($"Got value of type {actualType.Name} instead of {expectedType.Name}", textSpan);
+            Report($"Got value of type '{actualType.Name}' instead of '{expectedType.Name}'", textSpan);
         }
 
         internal void ReportInvalidNumberOfArguments(TextSpan textSpan, string functionName, int numOfArgs)
         {
-            Report($"Function {functionName} expects {numOfArgs} argument(s)", textSpan);
+            Report($"Function '{functionName}' expects {numOfArgs} argument(s)", textSpan);
         }
 
         internal void ReportInvalidArgumentType(TextSpan textSpan, int argCnt, PigeonType expectedType, PigeonType actualType)
         {
-            Report($"Argument {argCnt} should be of type {expectedType.Name} instead of {actualType.Name}", textSpan);
+            Report($"Argument {argCnt} should be of type '{expectedType.Name}' instead of '{actualType.Name}'", textSpan);
         }
 
         internal void ReportStatementNotInLoop(TextSpan textSpan, string statement)
         {
-            Report($"{statement} should be in a loop", textSpan);
+            Report($"'{statement}' should be in a loop", textSpan);
         }
 
         internal void ReportOutOfBounds(TextSpan textSpan, int index, int length)
         {
-            Report($"Index {index} out of bounds for length {length}", textSpan);
+            Report($"Index {index} is out of bounds for length {length}", textSpan);
         }
 
         public IEnumerator<CodeError> GetEnumerator()
